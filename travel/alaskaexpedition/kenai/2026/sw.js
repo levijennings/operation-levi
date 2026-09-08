a531aa64cab4d38abcd3b3041fdc9f3f01ab67da
levijennings <levi.jennings@me.com>
2026-09-07 23:36:49 -0700

    Add files via upload

--- travel/alaskaexpedition/kenai/2026/sw.js
+++ travel/alaskaexpedition/kenai/2026/sw.js
@@ -1,4 +1,4 @@
-const C='kenai26-v1';
+const C='kenai26-v2';
 const CORE=['./','./index.html','./averia-bold.woff2','./manifest.json','./icon-192.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
